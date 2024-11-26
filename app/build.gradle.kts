36bfb554e1105b6e229b15bc4e09b2e9006264f7
nadya <abclalala>
2024-11-26 20:49:25 +0700

    11. Shared Preferences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.picasso)
+    implementation(libs.gson)
 }
--- app/src/main/java/c14220230/paba/recyclerview/MainActivity.kt
+++ app/src/main/java/c14220230/paba/recyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package c14220230.paba.recyclerview
 
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -9,9 +10,34 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
+
+    //private lateinit var _nama : Array<String>
+    //private lateinit var _karakter : Array<String>
+    //private lateinit var _deskripsi : Array<String>
+    //private lateinit var _gambar : Array<String>
+
+    //private lateinit var _nama : MutableList<String>
+    //private lateinit var _karakter : MutableList<String>
+    //private lateinit var _deskripsi : MutableList<String>
+    //private lateinit var _gambar : MutableList<String>
+
+    private lateinit var sp: SharedPreferences
+
+    private var _nama: MutableList<String> = emptyList<String>().toMutableList()
+    private var _karakter: MutableList<String> = emptyList<String>().toMutableList()
+    private var _deskripsi: MutableList<String> = emptyList<String>().toMutableList()
+    private var _gambar: MutableList<String> = emptyList<String>().toMutableList()
+
+    private var arWayang = arrayListOf<wayang>()
+
+    private lateinit var _rvWayang: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,6 +47,27 @@ class MainActivity : AppCompatActivity() {
         SiapkanData()
         TambahData()
         TampilkanData()
+
+        sp = getSharedPreferences("dataSP", MODE_PRIVATE)
+
+        val gson = Gson()
+        val isiSP = sp.getString("spWayang", "null")
+        val type = object : TypeToken<ArrayList<wayang>>() {}.type
+        if (isiSP != "null") {
+            arWayang = gson.fromJson(isiSP, type)
+        }
+
+        if (arWayang.size == 0) {
+            SiapkanData()
+        } else {
+            arWayang.forEach {
+                _nama.add(it.nama)
+                _karakter.add(it.karakter)
+                _deskripsi.add(it.deskripsi)
+                _gambar.add(it.foto)
+            }
+            arWayang.clear()
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -42,6 +89,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun TambahData() {
+        val gson = Gson()
+        val editor = sp.edit()
         arWayang.clear()
         for (position in _nama.indices) {
             val data = wayang(
@@ -52,6 +101,10 @@ class MainActivity : AppCompatActivity() {
             )
             arWayang.add(data)
         }
+
+        val json = gson.toJson(arWayang)
+        editor.putString("spWayang", json)
+        editor.apply()
     }
 
     fun TampilkanData() {
@@ -60,7 +113,9 @@ class MainActivity : AppCompatActivity() {
 //        _rvWayang.layoutManager = StaggeredGridLayoutManager(
 //            2,
 //            LinearLayoutManager.VERTICAL)
-        _rvWayang.adapter = adapterRecView(arWayang)
+//        _rvWayang.adapter = adapterRecView(arWayang)
+
+        _rvWayang.layoutManager = LinearLayoutManager(this)
         val adapterWayang = adapterRecView(arWayang)
         _rvWayang.adapter = adapterWayang
 
@@ -80,7 +135,7 @@ class MainActivity : AppCompatActivity() {
             override fun delData(Pos: Int) {
                 AlertDialog.Builder(this@MainActivity)
                     .setTitle("HAPUS DATA")
-                    .setMessage("Apakah Benar Data "+_nama[Pos]+" Akan Dihapus?")
+                    .setMessage("Apakah Benar Data " + _nama[Pos] + " Akan Dihapus?")
                     .setPositiveButton(
                         "HAPUS",
                         DialogInterface.OnClickListener { dialog, which ->
@@ -106,17 +161,3 @@ class MainActivity : AppCompatActivity() {
         })
     }
 }
-
-//private lateinit var _nama : Array<String>
-//private lateinit var _karakter : Array<String>
-//private lateinit var _deskripsi : Array<String>
-//private lateinit var _gambar : Array<String>
-
-private lateinit var _nama : MutableList<String>
-private lateinit var _karakter : MutableList<String>
-private lateinit var _deskripsi : MutableList<String>
-private lateinit var _gambar : MutableList<String>
-
-private var arWayang = arrayListOf<wayang>()
-
-private lateinit var _rvWayang : RecyclerView
